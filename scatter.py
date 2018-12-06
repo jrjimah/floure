1c68e0e6433f9afc9f9207ba53ce9cd1e5665299
jrjimah <43221422+jrjimah@users.noreply.github.com>
2018-12-06 16:28:29 -0500

    Create scatter.py

---
+++ scatter.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+df  = pd.read_csv("data.csv")
+df.plot()  # plots all columns against index
+df.plot(kind='scatter',x='x',y='y') # scatter plot
+df.plot(kind='density')  # estimate density function
+# df.plot(kind='hist')  # histogram
